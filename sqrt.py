c86821439f2bfb6ea0b04d86671101258d7ca1a4
Requinard <d.diks@mail.blastedsolutions.com>
2014-11-21 16:15:39 +0100

    Added power functionality, comments and a max precision of 13

--- sqrt.py
+++ sqrt.py
@@ -1,11 +1,20 @@
+"""
+Try to find the square root of a number through approximation
 
-def sqrt(number, precision=13):
+Estimated operational time: O((number * 10)*precision)
+"""
+def sqrt(number, power=2,  precision=13):
     var = 1.0
     mod = 1.0
 
+    # 13 is the max amount of numbers we can actually count with floats, going above 13 is useless
+    if precision > 13:
+        precision = 13
+
+    # Loop throught the precision, each precision is a number behind the dot
     for i in range(0, precision):
         while True:
-            guess = var**2
+            guess = var**power
 
             if guess > number:
                 var -= mod
@@ -22,3 +31,6 @@ def sqrt(number, precision=13):
 
 print sqrt(9)
 print sqrt(10)
+print sqrt(520, 3)
+print sqrt(13.928)
+print sqrt(13.928, 2, 20)
